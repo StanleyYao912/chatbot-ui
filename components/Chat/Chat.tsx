45e8a48f3613505b413a9c9281331dbe3c00bbb8
Stanley <stanley@Stanleys-MacBook-Pro.local>
2023-07-10 18:46:43 +0800

    Changed title.

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,13 +352,13 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white mt=9">
-            Welcome to Stanley&apos;s Chatbot UI
+            Welcome to Stanley Yao Chatbot for Financial Industry
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`
 This open-source Chatbot, a milestone in my AI journey, harnesses OpenAI's LLM, Langchain, and Vector DB to provide advanced AI capabilities and updated, comprehensive financial data. Beyond conventional chat, ChatGPT offers an evolving, partially functional system. Enjoy this immersive AI experience!`}</div>
             <div className="mb-2 font-bold">
-              Important: Stanley&apos;s Chatbot is 100% unaffiliated with OpenAI.
+              Important: Stanley Yao Chatbot is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
